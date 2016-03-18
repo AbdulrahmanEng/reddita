addee7b77b936e9169c534cb7ac20389780d74bc
byDeinon <bydeinon@gmail.com>
2016-03-18 23:36:36 +0000

    Add infinite scrolling and pull refresh

--- www/js/app.js
+++ www/js/app.js
@@ -6,30 +6,54 @@
 angular.module('reditta', ['ionic', 'angularMoment'])
 
 .controller('redditaCtrl', function($scope, $http) {
-  $scope.stories = [];
-  $http.get('https://www.reddit.com/r/space.json')
-  .success(function(response) {
-    angular.forEach(response.data.children, function(child) {
-      $scope.stories.push(child.data);
-    })
-  })
+    $scope.stories = [];
+
+    function loadPosts(params, callback) {
+        $http.get('http://www.reddit.com/r/space/new/.json', { params: params })
+            .success(function(response) {
+              var stories = [];
+                angular.forEach(response.data.children, function(child) {
+                    stories.push(child.data);
+                });
+            callback(stories);      
+            });
+    }
+
+    $scope.loadNew = function() {
+      var params = {'before': $scope.stories[0].name};
+      loadPosts(params, function(newerPosts){
+        $scope.stories = newerPosts.concat($scope.stories);
+        $scope.broadcast('scroll.refreshComplete');
+      });
+    };
+
+    $scope.loadOld = function() {
+        var params = {};
+        if ($scope.stories.length > 0) {
+            params['after'] = $scope.stories[$scope.stories.length - 1].name;
+        }
+        loadPosts(params, function(olderPosts) {
+          $scope.stories = $scope.stories.concat(olderPosts);
+          $scope.$broadcast('scroll.infiniteScrollComplete');
+        });
+    };
+
 })
 
 .run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
-
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
