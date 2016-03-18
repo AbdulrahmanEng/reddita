2452c3c817d56688604c70f37734e44ec4df007c
byDeinon <bydeinon@gmail.com>
2016-03-18 22:48:06 +0000

    Add a time tag with moment.js and angular-moment

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('reditta', ['ionic'])
+angular.module('reditta', ['ionic', 'angularMoment'])
 
 .controller('redditaCtrl', function($scope, $http) {
   $scope.stories = [];
